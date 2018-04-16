f358db3f2bf5a3aa44c33164701e0233b243bac4
Bert Overduin <boverduin@gmail.com>
2018-04-16 23:30:20 +0100

    Minor corrections

--- public/app.js
+++ public/app.js
@@ -26,15 +26,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
   }
 
-const boba = ['Boba', 'Sock fluff', 'http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg']
-const barnaby = ['Barnaby', 'Tuna', 'https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg']
-const max = ['Max', 'Whiskas Temptations', 'http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg']
-const scratchy = ['Scratchy', 'Itchy', 'images/scratchy.png']
+  const boba = ['Boba', 'Sock fluff', 'http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg']
+  const barnaby = ['Barnaby', 'Tuna', 'https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg']
+  const max = ['Max', 'Whiskas Temptations', 'http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg']
+  const scratchy = ['Scratchy', 'Itchy', 'images/scratchy.png']
 
-const catArray = [boba, barnaby, max, scratchy];
+  const catArray = [boba, barnaby, max, scratchy];
 
-for (let i = 0; i < catArray.length; i++) {
-  createNewCat(catArray[i]);
-}
+  for (let i = 0; i < catArray.length; i++) {
+    createNewCat(catArray[i]);
+  }
 
 });
