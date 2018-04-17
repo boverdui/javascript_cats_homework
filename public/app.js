fd0ad1d3798ff28281e6054d0496115cd0158a43
Bert Overduin <boverduin@gmail.com>
2018-04-17 16:55:00 +0100

    Refactored after homework review

--- public/app.js
+++ public/app.js
@@ -8,15 +8,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const newCat = document.createElement('ul');
 
     const newCatName = document.createElement('li');
-    newCatName.textContent = `Name: ${cat[0]}`;
+    newCatName.textContent = `Name: ${cat.name}`;
 
     const newCatFood = document.createElement('li');
-    newCatFood.textContent = `Favourite food: ${cat[1]}`;
+    newCatFood.textContent = `Favourite food: ${cat.food}`;
 
     const newCatImage = document.createElement('li');
     const image = document.createElement('img');
     image.width = 500;
-    image.src = cat[2];
+    image.src = cat.src;
 
     cats.appendChild(newCat);
     newCat.appendChild(newCatName);
@@ -26,15 +26,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
   }
 
-  const boba = ['Boba', 'Sock fluff', 'http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg']
-  const barnaby = ['Barnaby', 'Tuna', 'https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg']
-  const max = ['Max', 'Whiskas Temptations', 'http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg']
-  const scratchy = ['Scratchy', 'Itchy', 'images/scratchy.png']
+  const boba = new Cat ('Boba', 'Sock fluff', 'images/boba.jpg')
+  const barnaby = new Cat ('Barnaby', 'Tuna', 'images/barnaby.jpg')
+  const max = new Cat ('Max', 'Whiskas Temptations', 'images/max.jpg')
+  const scratchy = new Cat ('Scratchy', 'Itchy', 'images/scratchy.png')
 
   const catArray = [boba, barnaby, max, scratchy];
 
-  for (let i = 0; i < catArray.length; i++) {
-    createNewCat(catArray[i]);
-  }
+  catArray.forEach(cat => createNewCat(cat));
 
 });
---
+++ public/cat.js
@@ -0,0 +1,5 @@
+const Cat = function (name, food, src) {
+  this.name = name;
+  this.food = food;
+  this.src = src;
+}
